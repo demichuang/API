b5cd9c99d841803a77676e23d353491988406ba9
hi <demichuang40207@gmail.com>
2016-08-25 22:43:57 +0000

    my modify

--- Api.php
+++ Api.php
@@ -33,20 +33,28 @@ class Api extends Connect
     {
         $stmt = $this->db->prepare("SELECT `balance` FROM `user` WHERE `username` = '$name'");
         $stmt->execute();
-        $balance = $stmt->fetchColumn();
+        $rowNum = $stmt->rowCount();
 
-        $tempArray = array("result" => true, "data" => ["action" => "getBalance", "balance" => $balance]);
-        echo json_encode($tempArray);
+        if ($rowNum != 1) {
+            $tempArray = array("result" => false, "data" => ["action" => "addUser", "Error" => "No this user."]);
+            echo json_encode($tempArray);
+        }
+
+        if ($rowNum == 1) {
+            $balance = $stmt->fetchColumn();
+            $tempArray = array("result" => true, "data" => ["action" => "getBalance", "balance" => $balance]);
+            echo json_encode($tempArray);
+        }
     }
 
-    function transferIn($name, $money, $transid)
+    function in($name, $money, $transid)
     {
         $stmt = $this->db->prepare("SELECT * FROM `record` WHERE `transid` = '$transid'");
         $stmt->execute();
         $rowNum = $stmt->rowCount();
 
         if ($rowNum == 1) {
-            $tempArray = array("result" => false, "data" => ["action" => "transferIn", "Error" => "Transid Repeat"]);
+            $tempArray = array("result" => false, "data" => ["action" => "in", "Error" => "Transid Repeat"]);
             echo json_encode($tempArray);
         }
 
@@ -56,19 +64,19 @@ class Api extends Connect
             $stmt = $this->db->prepare("INSERT `record`(`username`, `transid`, `transfer`) VALUES ('$name', '$transid','input:$money')");
             $stmt->execute();
 
-            $tempArray = array("result" => true, "data" => ["action" => "transferIn", "username" => $name, "transid" => $transid, "money" => $money]);
+            $tempArray = array("result" => true, "data" => ["action" => "in", "username" => $name, "transid" => $transid, "money" => $money]);
             echo json_encode($tempArray);
         }
     }
 
-    function transferOut($name, $money, $transid)
+    function out($name, $money, $transid)
     {
         $stmt = $this->db->prepare("SELECT * FROM `record` WHERE `transid` = '$transid'");
         $stmt->execute();
         $rowNum = $stmt->rowCount();
 
         if ($rowNum == 1) {
-            $tempArray = array("result" => false, "data" => ["action" => "transferOut", "Error" => "Transid Repeat"]);
+            $tempArray = array("result" => false, "data" => ["action" => "out", "Error" => "Transid Repeat"]);
             echo json_encode($tempArray);
         }
 
@@ -83,12 +91,12 @@ class Api extends Connect
                 $stmt = $this->db->prepare("INSERT `record`(`username`, `transid`, `transfer`) VALUES ('$name', '$transid','output:$money')");
                 $stmt->execute();
 
-                $tempArray = array("result" => true, "data" => ["action" => "transferOut", "username" => $name, "transid" => $transid, "money" => $money]);
+                $tempArray = array("result" => true, "data" => ["action" => "out", "username" => $name, "transid" => $transid, "money" => $money]);
                 echo json_encode($tempArray);
             }
 
             if ($balance < $money) {
-                $tempArray = array("result" => false, "data" => ["action" => "transferOut", "Error" => "Money not enough"]);
+                $tempArray = array("result" => false, "data" => ["action" => "out", "Error" => "Money not enough"]);
                 echo json_encode($tempArray);
             }
         }
@@ -98,34 +106,106 @@ class Api extends Connect
     {
         $stmt = $this->db->prepare("SELECT `transfer` FROM `record` WHERE `transid` = '$transid'");
         $stmt->execute();
-        $status = $stmt->fetchColumn();
+        $rowNum = $stmt->rowCount();
 
-        $tempArray = array("result" => true, "data" => ["action" => "getStatus", "transid" => $transid, "Transid_action" => $status]);
-        echo json_encode($tempArray);
+        if ($rowNum != 1) {
+            $tempArray = array("result" => false, "data" => ["action" => "getStatus", "Error" => "No this transid."]);
+            echo json_encode($tempArray);
+        }
+
+        if ($rowNum == 1) {
+            $status = $stmt->fetchColumn();
+            $tempArray = array("result" => true, "data" => ["action" => "getStatus", "transid" => $transid, "Transid_action" => $status]);
+            echo json_encode($tempArray);
+        }
     }
 }
 
 $myApi = new Api();
 
+if (!isset($_GET["action"])) {
+    $tempArray = array("result" => false, "data" => ["action" => "", "Error" => "No Action Enter"]);
+    echo json_encode($tempArray);
+}
 
 if (isset($_GET["action"])) {
+    if ($_GET["action"] != "addUser" || $_GET["action"] != "getBalance" || $_GET["action"] != "in" || $_GET["action"] != "out" || $_GET["action"] != "getStatus") {
+        $tempArray = array("result" => false, "data" => ["action" => "", "Error" => "Action Enter false"]);
+        echo json_encode($tempArray);
+    }
+
     if ($_GET["action"] == "addUser") {
-        $myApi->addUser($name);
+        if (!isset($name)) {
+            $tempArray = array("result" => false, "data" => ["action" => "addUser", "Error" => "You don't have name parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (isset($name)) {
+            $myApi->addUser($name);
+        }
     }
 
     if ($_GET["action"] == "getBalance") {
-        $myApi->getBalance($name);
+        if (!isset($name)) {
+            $tempArray = array("result" => false, "data" => ["action" => "getBalance", "Error" => "You don't have name parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (isset($name)) {
+            $myApi->getBalance($name);
+        }
     }
 
-    if ($_GET["action"] == "transferIn") {
-        $myApi->transferIn($name, $money, $transid);
+    if ($_GET["action"] == "in") {
+        if (!isset($name)) {
+            $tempArray = array("result" => false, "data" => ["action" => "in", "Error" => "You don't have name parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (!isset($money)) {
+            $tempArray = array("result" => false, "data" => ["action" => "in", "Error" => "You don't have money parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (!isset($transid)) {
+            $tempArray = array("result" => false, "data" => ["action" => "in", "Error" => "You don't have transid parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (isset($name) && isset($money) && isset($transid)) {
+            $myApi->in($name, $money, $transid);
+        }
     }
 
-    if ($_GET["action"] == "transferOut") {
-        $myApi->transferOut($name, $money, $transid);
+    if ($_GET["action"] == "out") {
+        if (!isset($name)) {
+            $tempArray = array("result" => false, "data" => ["action" => "out", "Error" => "You don't have name parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (!isset($money)) {
+            $tempArray = array("result" => false, "data" => ["action" => "out", "Error" => "You don't have money parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (!isset($transid)) {
+            $tempArray = array("result" => false, "data" => ["action" => "out", "Error" => "You don't have transid parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (isset($name) && isset($money) && isset($transid)) {
+            $myApi->out($name, $money, $transid);
+        }
     }
 
     if($_GET["action"] == "getStatus") {
-        $myApi->getStatus($transid);
+        if (!isset($transid)) {
+            $tempArray = array("result" => false, "data" => ["action" => "getStatus", "Error" => "You don't have transid parameter."]);
+            echo json_encode($tempArray);
+        }
+
+        if (isset($transid)) {
+            $myApi->getStatus($transid);
+        }
     }
 }
